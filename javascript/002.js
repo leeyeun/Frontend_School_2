5d1469fe87a10ed0a9fb62c452cddee235aa9671
leeyeun <dpdms0109@naver.com>
2022-04-27 19:59:19 +0900

    04/27

---
+++ javascript/002.js
@@ -0,0 +1,4 @@
+document.getElementById("one").innerHTML= "hello <strong>world document </strong>";
+console.log("hello world conslog");
+window.alert("hello world alert");
+document.write("hello!");
