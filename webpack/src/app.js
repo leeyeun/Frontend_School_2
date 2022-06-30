e5e221a41a435567f1bb478f92e334b0f41891af
leeyeun <dpdms0109@naver.com>
2022-06-30 16:05:28 +0900

    06/30

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -1,3 +1,18 @@
 import { plus } from "./plus.js";
+import "./style.css";
+import tiger from "./tiger.png";
 
-console.log(plus(2, 4));
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.innerHTML = `<img src = "${tiger}">`;
+});
+
+// console.log(pw);
+
+let env;
+if (process.env.NODE_ENV === "development") {
+    env = dev;
+} else {
+    env = pro;
+}
+
+console.log(env);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,60 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const dotenv = require("dotenv");
+dotenv.config();
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+console.log("nodeEnv" + process.env.NODE_ENV);
 module.exports = {
-    mode: "development",
+    mode: process.env.NODE_ENV === "development" ? "development" : "production",
     entry: {
         main: path.resolve("./src/app.js"),
     },
     output: {
+        // publicPath: "/webpack/dist/",
         filename: "[name].js",
         path: path.resolve("./dist"),
     },
+    module: {
+        rules: [
+            // {
+            //     test: /\.js$/,
+            //     use: [path.resolve("./myLoader.js")],
+            // },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|jped|gif|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 1kb가 1024byte 이기 때문에 20kb를 원한다면 1024에 20을 곱합니다.
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `Commit version: ${childProcess.execSync(
+                "git rev-parse --short HEAD"
+            )}
+                Committer name : ${childProcess.execSync(
+                    "git config user.name"
+                )}
+                commit Date : ${new Date().toLocaleString()}`,
+        }),
+        new webpack.DefinePlugin({
+            dev: JSON.stringify(process.env.DEV_API),
+            pro: JSON.stringify(process.env.PRO_API),
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+    ],
 };
